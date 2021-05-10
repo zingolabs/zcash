e73ab6e32a44a7ec6dd503b51ad9a41d29912a16
dannasessha <sessha@zingolabs.com>
2021-05-10 19:50:27 +0000

    cleanup verifychain

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -887,7 +887,7 @@ UniValue verifychain(const UniValue& params, bool fHelp)
     if (fHelp || params.size() > 2) {
         HelpSections help_sections =
             HelpSections(__func__)
-                .set_usage("verifychain ( checklevel numblocks )")
+                .set_usage("( checklevel numblocks )")
                 .set_description("Verifies blockchain database.\n")
                 .set_arguments("1. checklevel   (numeric, optional, 0-4, default=" +
                                strprintf("%d", nCheckLevel) + ") How thorough the block verification is.\n"
